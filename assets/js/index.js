63b4f7bad3c471fd487fab91efa4b4124d096db8
Elena Senicheva <elenasenicheva81@gmail.com>
2021-03-23 09:53:33 +0200

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+// alert("Hello world!");
+// alert("Bye world!");
+
+// const helloStr='Hello World';
+// let test;
+// test='Test';
+// test=5;
+// console.log(helloStr);
+// console.log(test, typeof test);
+
+// let test ='5' + '5';
+// if(test){
+//   console.log('Hello world!');
+// }
+// console.log('Hello world!');
+// console.log("test");
+// console.log("Hello world!");
+
+// console.log("10");
+// console.log(10);
+
+// console.log(true);
+// console.log(null);
+// console.log(undefined);
+
+const a=5;
+const b=6;
+console.log(a*b);
+
+const c=5;
+const d=6;
+console.log(c/d);
+
+const e=5;
+const f=6;
+console.log(e+f);
+
+const number=11;
+const bol=true;
+const text="java script";
+const test="100";
+
+console.log(number,bol,text,test);
+
+let num=1;
+
+num+=11;
+num-=11;
+num*=11;
+num/=11;
+num++;
+num--;
+
+console.log(num);
+
